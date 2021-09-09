69b6882f2d3056dd5711b53f37e3d629008bf6b8
kseniapolonsky <ksenia.polonsky@gmail.com>
2021-09-09 13:15:48 +0300

    updated the dp algo

--- main.py
+++ main.py
@@ -33,18 +33,6 @@ def main_func():
     print(seed)
 
 #   Ksenia's: running Pepsurf with the seed found and given arguments to find the preliminary cluster
-#     seed = "QFTAAE"
-#     seed = "QMQFTAAEMGQYT"
-#     seed = "QFTAAE"
-#     seed = "QFTAAEEC"
-#     seed = 'NSPRVIR'
-#     seed = 'MKLPRTNQ'
-#     seed = 'RLVRPTQ'
-#     seed = 'FYINAL'
-#     seed = 'FYIPAEI'
-#     seed = 'LRLRNRPTQ'
-    # seed = 'LRNRPTQ'
-    # seed = 'LRNTQRP'
     # seed = 'LRTQRNRP'
     # seed = 'TNRLRN'
     print(seed)
@@ -60,18 +48,6 @@ def main_func():
     # paths_set = [['N183', 'W184', 'T188', '', 'A194', '', '', 'C198'], ['N74', '', '', 'A77', 'A78', 'D81', 'E79'], ['N120', '', 'T48', '', 'A47', 'D51', ''], ['Q219', '', 'T216', 'A217', '', 'D197', '', 'C198'], ['N21', '', 'T19', 'A22', '', 'E29', 'E28'], ['N193', '', 'T171', 'A174', '', 'D166', 'D163'], ['', 'T210', 'A208', 'A209', 'E213', 'E212'], ['', '', 'T186', '', 'A177', '', 'E180']]
     # input_alignment_set = [['X', 'Z', 'O', 'A', 'A', 'J', 'J', 'C'], ['X', 'Z', 'O', 'A', 'A', 'J', 'J'], ['X', 'Z', 'O', 'A', 'A', 'J', 'J'], ['X', 'Z', 'O', 'A', 'A', 'J', 'J', 'C'], ['X', 'Z', 'O', 'A', 'A', 'J', 'J'], ['X', 'Z', 'O', 'A', 'A', 'J', 'J'], ['Z', 'O', 'A', 'A', 'J', 'J'], ['X', 'Z', 'O', 'A', 'A', 'J', 'J']]
     # output_alignment_set = [['X', 'Z', 'O', 'U', 'A', 'X', 'X', 'C'], ['X', 'U', '-', 'A', 'A', 'J', 'J'], ['X', 'M', 'O', 'G', 'A', 'J', 'X'], ['X', 'M', 'O', 'A', 'U', 'J', 'B', 'C'], ['X', 'U', 'O', 'A', 'U', 'J', 'J'], ['X', 'U', 'O', 'A', 'B', 'J', 'J'], ['M', 'O', 'A', 'A', 'J', 'J'], ['J', 'Y', 'O', 'B', 'A', 'X', 'J']]
-
-    # paths_set =  [['Q396', '', '', 'R395', 'V394', '', 'R495'], ['', 'S461', 'P462', '', '', 'L443', 'R444'], ['', '', 'P450', 'R449', '', '', 'R342']]
-    # input_alignment_set = [['X', 'O', 'P', 'B', 'U', 'U', 'B'], ['X', 'O', 'P', 'B', 'U', 'U', 'B'], ['X', 'O', 'P', 'B', 'U', 'U', 'B']]
-    # output_alignment_set = [['X', 'A', '-', 'B', 'U', 'Y', 'B'], ['B', 'O', 'P', '-', 'Z', 'U', 'B'], ['J', '-', 'P', 'B', '-', 'Z', 'B']]
-
-    # paths_set = [['R444', 'L443', '', '', 'P462', 'S461', '', '', 'C467']]
-    # input_alignment_set = [['B', 'U', 'U', 'B', 'P', 'O', 'X', 'B', 'C']]
-    # output_alignment_set = [['B', 'U', 'Z', '-', 'P', 'O', 'B', '-', 'C']]
-
-    # paths_set = [['G110', '', 'W112', 'F113', 'H114', '', 'R98', 'D118', ''], ['W120', 'G121', '', '', '', '', '', 'R39', 'E47', 'A45'], ['', 'W112', '', 'H33', 'D32', 'R29', 'D27', ''], ['W120', 'G121', 'Q122']]
-    # input_alignment_set = [['G', 'X', 'Z', 'Z', 'H', 'J', 'B', 'J', 'A'], ['Z', 'G', 'X', 'Z', 'Z', 'H', 'J', 'B', 'J', 'A'], ['X', 'Z', 'Z', 'H', 'J', 'B', 'J', 'A'], ['Z', 'G', 'X']]
-    # output_alignment_set =[['G', 'J', 'Z', 'Z', 'H', 'X', 'B', 'J', 'G'], ['Z', 'G', 'J', '-', 'Y', '-', 'X', 'B', 'J', 'A'], ['B', 'Z', 'U', 'H', 'J', 'B', 'J', 'O'], ['Z', 'G', 'X']]
 
 #   Ksenia's: return set of dictionaries, 1 for each path
     set_of_dicts = Seed_graph.path_to_graph_dictionary(graph, paths_set, input_alignment_set, output_alignment_set, original_seed, seed)
